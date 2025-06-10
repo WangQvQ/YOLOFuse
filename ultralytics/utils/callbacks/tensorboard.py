b04a695f55c10e4e70c9d7ef47262f5a2d8861cb
WangQvQ <1579093407@qq.com>
2025-06-10 13:20:31 +0800

    Update tensorboard.py

--- ultralytics/utils/callbacks/tensorboard.py
+++ ultralytics/utils/callbacks/tensorboard.py
@@ -36,7 +36,8 @@ def _log_tensorboard_graph(trainer):
     imgsz = trainer.args.imgsz
     imgsz = (imgsz, imgsz) if isinstance(imgsz, int) else imgsz
     p = next(trainer.model.parameters())  # for device, type
-    im = torch.zeros((1, 3, *imgsz), device=p.device, dtype=p.dtype)  # input image (must be zeros, not empty)
+    channels = getattr(trainer.args, "ch", 3)
+    im = torch.zeros((1, channels, *imgsz), device=p.device, dtype=p.dtype)  # input image (must be zeros, not empty)
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=UserWarning)  # suppress jit trace warning
